25e2d81e0073b5b24c80cb2048e7cf4823f940bd
jocelinqueau <queau.j@gmail.com>
2023-06-05 21:20:15 +0200

    refresh strategy with time now

--- src/services/spotify/getFetcher.ts
+++ src/services/spotify/getFetcher.ts
@@ -1,4 +1,6 @@
-export async function getWithBackoffRetry({
+import { refreshAccessToken } from './refreshAccessToken';
+
+export async function getWithBackoffRetry<T>({
   url,
   maxRetries,
   retryDelay,
@@ -6,7 +8,7 @@ export async function getWithBackoffRetry({
   url: string;
   maxRetries: number;
   retryDelay: number;
-}): Promise<Response> {
+}): Promise<T> {
   let retries = 0;
   let response: Response | undefined;
 
@@ -21,9 +23,24 @@ export async function getWithBackoffRetry({
         },
       });
 
+      const body = await response.json();
+      console.log('la', body);
+      if (
+        response.status === 401 &&
+        (body.error.message === 'The access token expired' ||
+          body.error.message === 'Invalid access token')
+      ) {
+        const { access_token, expires_in, scope, token_type } =
+          await refreshAccessToken();
+        localStorage.setItem('access_token', access_token);
+        localStorage.setItem('expires_in', expires_in);
+        localStorage.setItem('scopes', scope.split(' ').join(','));
+        localStorage.setItem('token_type', token_type);
+        localStorage.setItem('now', new Date().toString());
+      }
       if (response.status !== 429) {
         // If the response is not a 429 error, return the response
-        return response;
+        return body;
       }
     } catch (error) {
       console.error('Request failed:', error);
