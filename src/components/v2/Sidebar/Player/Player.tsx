5c0c3d8833983aaf977397543dd9744b26ae7ba4
jocelin queau <queau.j@gmail.com>
2023-07-03 09:24:48 +0200

    bug

--- src/components/v2/Sidebar/Player/Player.tsx
+++ src/components/v2/Sidebar/Player/Player.tsx
@@ -13,6 +13,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShuffle, faRepeat } from '@fortawesome/free-solid-svg-icons';
 import { GenresTag } from '../../GenresTag';
+import Marquee from 'react-fast-marquee';
 
 const PlayerV2Styles = styled.div`
   width: 240px;
@@ -170,11 +171,19 @@ export const PlayerV2 = () => {
         <div className="track-infos">
           <ChevronRightIcon />
           <div className="track-infos__text">
+          <Marquee
+              speed={15}
+              pauseOnHover={true}
+              gradient={true}
+              gradientColor={[27, 35, 38]}
+              gradientWidth={10}
+            >
             <span className="track-infos__text__title">Decrescendo</span>
             <span>/</span>
             <span className="track-infos__text__artist">Lomepal</span>
             <span>/</span>
             <span className="track-infos__text__album">Mauvais ordre</span>
+          </Marquee>
           </div>
         </div>
         <div className="leftovers">
@@ -189,4 +198,4 @@ export const PlayerV2 = () => {
       </div>
     </PlayerV2Styles>
   );
-};
+}; 
--- src/layouts/v2.tsx
+++ src/layouts/v2.tsx
@@ -1,17 +1,41 @@
+'use client';
+
 import { Links } from '@/components/v2/Sidebar/Links';
 import { PlayerV2 } from '@/components/v2/Sidebar/Player/Player';
 import { ReactNode } from 'react';
+import { Panel, PanelGroup } from 'react-resizable-panels';
 
-export const V2 = ({ children }: { children: ReactNode }) => {
+
+
+export const V2 = ({ children, defaultLayout = [33, 67] }: { children: ReactNode, defaultLayout?: number[] | undefined }) => {
+  const onLayout = (sizes: number[]) => {
+    document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`;
+  };
+
+  let storage;
+  try {
+    storage = localStorage;
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <div className="bg-[#0F1112] w-screen h-screen p-[0.6rem] flex gap-[0.3rem]">
-      <div className="left flex flex-col gap-[0.3rem] h-full">
-        <PlayerV2 />
-        <Links />
-      </div>
-      <div className="right px-[20px] py-[40px] bg-[#1b2326] flex-1 rounded-[10px]">
-        {children}
-      </div>
+      <PanelGroup onLayout={onLayout} direction="horizontal">
+        <Panel  minSize={20} defaultSize={defaultLayout[0]}>
+          <div className="left flex flex-col gap-[0.3rem] h-full">
+            <PlayerV2 />
+            <Links />
+          </div>
+        </Panel>
+        <Panel minSize={50} defaultSize={defaultLayout[1]}>
+          <div className="right px-[20px] py-[40px] bg-[#1b2326] flex-1 rounded-[10px] h-full">
+            {children}
+          </div>
+        </Panel>
+      </PanelGroup>
     </div>
   );
 };
+
+export default V2;
